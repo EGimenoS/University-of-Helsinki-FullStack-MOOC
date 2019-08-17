0a0612320e38d32a189423315854c5e3f7fd84b1
Ernesto Gimeno <ergisan@gmail.com>
2019-08-17 20:19:51 +0200

    1.6 step 1 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,19 +1,58 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const Statistics = ({ good, neutral, bad }) => {
+	return (
+		<>
+			<p>
+				Good <span>{good}</span>
+			</p>
+			<p>
+				Neutral <span>{neutral}</span>
+			</p>
+			<p>
+				Bad <span>{bad}</span>
+			</p>
+		</>
+	);
+};
+
+const Button = ({ clickHandler, text }) => {
+	return (
+		<>
+			<button onClick={clickHandler}>{text}</button>
+		</>
+	);
+};
 
 const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+	// save clicks of each button to own state
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
 
-  return (
-    <div>
-      code here
-    </div>
-  )
-}
+	const addToGood = () => {
+		setGood(good + 1);
+	};
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+	const addToNeutral = () => {
+		setNeutral(neutral + 1);
+	};
+
+	const addToBad = () => {
+		setBad(bad + 1);
+	};
+
+	return (
+		<div>
+			<h1>Give FeedBack</h1>
+			<Button clickHandler={addToGood} text="good" />
+			<Button clickHandler={addToNeutral} text="neutral" />
+			<Button clickHandler={addToBad} text="bad" />
+			<h1>Statistics</h1>
+			<Statistics good={good} neutral={neutral} bad={bad} />
+		</div>
+	);
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
