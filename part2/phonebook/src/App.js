ef389027ad2b24991adc2d98e300858936c51ac0
Ernesto Gimeno <ergisan@gmail.com>
2019-08-18 17:23:58 +0200

    2.9 step 4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,55 +1,76 @@
 import React, { useState } from "react";
-import Person from './components/Person'
+import Person from "./components/Person";
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: "Arto Hellas", number: '555-141567' }]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("")
+	const [persons, setPersons] = useState([
+		{ name: "Arto Hellas", number: "040-123456" },
+		{ name: "Ada Lovelace", number: "39-44-5323523" },
+		{ name: "Dan Abramov", number: "12-43-234345" },
+		{ name: "Mary Poppendieck", number: "39-23-6423122" }
+	]);
+	const [newName, setNewName] = useState("");
+	const [newNumber, setNewNumber] = useState("");
+	const [showNames, setShowNames] = useState("");
 
-  const isDuplicated = (name) => {
-    return persons.some(person => person.name === name)
-  }
-  
-  const addContact = (event) => {
-    event.preventDefault()
+	const isDuplicated = name => {
+		return persons.some(person => person.name === name);
+	};
 
-    if (isDuplicated(newName)) {
-      alert(`${newName} is already in the Phonebook`)
-      return
-    }
-    
-    const newContact = {
-      name: newName,
-      number: newNumber
-    }
-    setPersons(persons.concat(newContact))
-    setNewName('')
-  }
+	const addContact = event => {
+		event.preventDefault();
 
-  const handleChangeName = (event) => {
-    setNewName(event.target.value)
-  }
+		if (isDuplicated(newName)) {
+			alert(`${newName} is already in the Phonebook`);
+			return;
+		}
 
-  const handleChangeNumber = (event) => {
-    setNewNumber(event.target.value)
-  }
+		const newContact = {
+			name: newName,
+			number: newNumber
+		};
+		setPersons(persons.concat(newContact));
+    setNewName("");
+    setNewNumber("");
+	};
+
+	const handleChangeName = event => {
+		setNewName(event.target.value);
+	};
+
+	const handleChangeNumber = event => {
+		setNewNumber(event.target.value);
+	};
+
+	const handleSearch = event => {
+		setShowNames(event.target.value);
+	};
 
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<form onSubmit={addContact} >
+			<div>
+				Filter by name:{" "}
+				<input value={showNames} onChange={handleSearch} type="text" />
+			</div>
+			<h3>Add a new Contact</h3>
+			<form onSubmit={addContact}>
 				<div>
 					name: <input value={newName} onChange={handleChangeName} />
 				</div>
-        <div>number: <input value={newNumber} onChange={handleChangeNumber}/></div>
+				<div>
+					number: <input value={newNumber} onChange={handleChangeNumber} />
+				</div>
 				<div>
 					<button type="submit">add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			{persons.map((person) => {
-        return <Person key={person.name} person={person} />
-      })}
+      {persons
+        .map(person => Object.assign(person, {nameToSearch: person.name.toLowerCase()}) ) //object spread operator throwing an error? {...person, nameToSearch...
+				.filter(person => person.nameToSearch.includes(showNames.toLowerCase()))
+				.map(person => {
+					return <Person key={person.name} person={person} />;
+				})}
 		</div>
 	);
 };
