e74ccd4aeed9569b1ffe72cd0ac9515bb14c6f49
Ernesto Gimeno <ergisan@gmail.com>
2019-08-18 22:14:58 +0200

    2.13 step 2

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
+import '../main.css'
 
 const Country = ({ country, detailed }) => {
+  const [viewDetailed, setViewDetailed] = useState(false)
+  const toggleView = () => {
+    setViewDetailed(true)
+  }
 	const detailView = () => {
 		return (
-    <>
-    <h1>{country.name}</h1>
-    <p>Capital: {country.capital}</p>
-    <p>Population: {country.population} </p>
-    <h2>Languages</h2>
-    <ul>
-      {country.languages.map(language => <li>{language.name}</li>)}
-    </ul>
-    <img src={country.flag}></img>
-    </>
-    );
+			<>
+				<h1>{country.name}</h1>
+				<p>Capital: {country.capital}</p>
+				<p>Population: {country.population} </p>
+				<h2>Languages</h2>
+				<ul>
+					{country.languages.map(language => (
+						<li key={language.name}>{language.name}</li>
+					))}
+				</ul>
+				<img src={country.flag} alt={`the flag from ${country.name}`} />
+			</>
+		);
 	};
 	const regularView = () => {
-		return <p>{country.name}</p>;
+		return (
+			<>
+				<div>
+					{country.name}
+					<button onClick={toggleView}>View</button>
+          { viewDetailed ? detailView() : ''}
+				</div>
+			</>
+		);
 	};
 	return <>{detailed ? detailView() : regularView()}</>;
 };
