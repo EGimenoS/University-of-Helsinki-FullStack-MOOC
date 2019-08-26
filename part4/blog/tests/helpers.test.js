a8a0f4e32519b547bab6858de7db8ed6ab23aa75
EgimenoS <ergisan@gmail.com>
2019-08-26 16:57:30 +0200

    4.4 step 2

--- part4/blog/tests/helpers.test.js
+++ part4/blog/tests/helpers.test.js
@@ -1,6 +1,14 @@
 const listHelper = require('../utils/list_helper')
+const blogs = require('../utils/bloglist_test').blogs
 
-test('dummy returns one', () => {
-  const blogs = []
-  expect(listHelper.dummy()).toBe(1)
+
+describe('total likes', () => {
+  const listWithOneBlog = blogs.slice(0,1)
+  test('when list has only one blog equals the likes of that', () => {
+    expect(listHelper.totalLikes(listWithOneBlog)).toBe(7)
+  })
+
+  test('it equals to the sum of likes in the blog list', () => {
+    expect(listHelper.totalLikes(blogs)).toBe(36)
+  })
 })
