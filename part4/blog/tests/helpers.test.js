3a27ff42eeec60aeee5f2e2d782c613632bba5b6
EgimenoS <ergisan@gmail.com>
2019-08-27 12:26:44 +0200

    4.5 step3

--- part4/blog/tests/helpers.test.js
+++ part4/blog/tests/helpers.test.js
@@ -1,14 +1,24 @@
-const listHelper = require('../utils/list_helper')
-const blogs = require('../utils/bloglist_test').blogs
+const listHelper = require("../utils/list_helper");
+const blogs = require("../utils/bloglist_test").blogs;
 
+describe("total likes", () => {
+  const listWithOneBlog = blogs.slice(0, 1);
+  test("when list has only one blog equals the likes of that", () => {
+    expect(listHelper.totalLikes(listWithOneBlog)).toBe(7);
+  });
 
-describe('total likes', () => {
-  const listWithOneBlog = blogs.slice(0,1)
-  test('when list has only one blog equals the likes of that', () => {
-    expect(listHelper.totalLikes(listWithOneBlog)).toBe(7)
-  })
+  test("it equals to the sum of likes in the blog list", () => {
+    expect(listHelper.totalLikes(blogs)).toBe(36);
+  });
+});
 
-  test('it equals to the sum of likes in the blog list', () => {
-    expect(listHelper.totalLikes(blogs)).toBe(36)
-  })
-})
+describe("blog with the most likes", () => {
+  const mostLikedBlog = {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    likes: 12
+  };
+  test("it returns the blog with the most likes", () => {
+    expect(listHelper.getMostLikedBlog(blogs)).toEqual(mostLikedBlog);
+  });
+});
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,9 +1,22 @@
 const totalLikes = blogs => {
   return blogs.reduce((sum, blog) => {
-    return sum + blog.likes
-  }, 0)
-}
+    return sum + blog.likes;
+  }, 0);
+};
+
+const getMostLikedBlog = blogs => {
+  return blogs
+    .map(blog => {
+      return { title: blog.title, author: blog.author, likes: blog.likes };
+    })
+    .reduce((mostLiked, blog) => {
+      if (blog.likes > mostLiked.likes) {
+        return blog;
+      } else return mostLiked;
+    });
+};
 
 module.exports = {
-  totalLikes
+  totalLikes,
+  getMostLikedBlog
 };
