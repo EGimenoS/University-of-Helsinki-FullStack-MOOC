2579cc822d7725cd032b95c7a68650c70b3c9a01
EgimenoS <ergisan@gmail.com>
2019-08-29 12:52:13 +0200

    4.10

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
-const Blog = require('../models/blog')
+const Blog = require("../models/blog");
 
 const api = supertest(app);
-const initialBlogs = require('../utils/bloglist_test').blogs
+const initialBlogs = require("../utils/bloglist_test").blogs;
 
-beforeEach(async() => {
-  await Blog.deleteMany({})
+beforeEach(async () => {
+  await Blog.deleteMany({});
   for (const blog of initialBlogs) {
-    let blogObject = new Blog(blog)
-    await blogObject.save()
+    let blogObject = new Blog(blog);
+    await blogObject.save();
   }
-})
+});
 
 describe("the response is correct when hitting GET /blogs", () => {
   test("blogs are returned as JSON", async () => {
@@ -23,15 +23,37 @@ describe("the response is correct when hitting GET /blogs", () => {
   });
 
   test("it returns the correct number of blogs", async () => {
-    const response = await api.get("/api/blogs")
+    const response = await api.get("/api/blogs");
     expect(response.body.length).toBe(initialBlogs.length);
-  })
+  });
 
   test("id property is correctly named as id, not _id", async () => {
-    const response = await api.get("/api/blogs")
-    console.log(response.body[0].id)
-    expect(response.body[0].id).toBeDefined()
-  })
+    const response = await api.get("/api/blogs");
+    console.log(response.body[0].id);
+    expect(response.body[0].id).toBeDefined();
+  });
+});
+
+describe("new Blogs can be added correctly", () => {
+  const newBlog = {
+    title: "Angular University",
+    author: "Vasco",
+    url: "blog.angularuniversity.io",
+    likes: 0
+  };
+  test("when addding a blog, the blog count increases", async () => {
+    await api.post("/api/blogs").send(newBlog);
+
+    const response = await api.get("/api/blogs");
+    expect(response.body.length).toBe(initialBlogs.length + 1);
+  });
+  test("when addding a blog, the title matches", async () => {
+    await api.post("/api/blogs").send(newBlog);
+
+    const response = await api.get("/api/blogs");
+    const titles = response.body.map(blog => blog.title);
+    expect(titles).toContain(newBlog.title);
+  });
 });
 
 afterAll(() => {
