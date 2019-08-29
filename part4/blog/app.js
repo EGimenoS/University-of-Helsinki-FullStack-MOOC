c5e6aed6b5814d12d085f064fcf46885327d176b
EgimenoS <ergisan@gmail.com>
2019-08-29 15:39:52 +0200

    4.15

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users")
 const mongoose = require("mongoose");
 
 console.log("connecting to", config.MONGODB_URI);
@@ -21,5 +22,6 @@ app.use(cors());
 app.use(express.static("build"));
 app.use(bodyParser.json());
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter)
 
 module.exports = app;
